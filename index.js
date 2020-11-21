36a2a8e64209f4ccdebf85520a4efa9109c3f713
jeongks <kisuk.jeong.lds@gmail.com>
2020-11-22 00:26:10 +0900

    there is problem

--- index.js
+++ index.js
@@ -1,28 +1,91 @@
+//enabling express
+const { WSAEACCES } = require('constants');
+const { Pool } = require('pg');
 const express = require('express');
+//enabling static path
 const path = require('path');
+const { response } = require('express');
 const PORT= process.env.PORT || 5000;
+require('dotenv').config();
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({connectionString: connectionString});
 
 express()
     .use(express.static(path.join(__dirname,'public')))
     .set('views',path.join(__dirname,'views'))
     .set('view engine', 'ejs')
-    .get('/', (req,res) => res.render(index))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .post('/', (req,res) => res.render(index))
+    .post('/calc',formInput)
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 function formInput(request, response){
-    let gender= request.query.gender;
+    let gender = Number(request.query.gender);
     let unit = request.query.measureUnit;
-    let height = request.query.height;
-    let waist = request.query.waist;
-    let hours = request.query.hours
-    let part = request.query.preferredPart;
+    let height = Number(request.query.height);
+    let waist = Number(request.query.waist);
+    let hours = Number(request.query.hours);
+    let preferredPart = request.query.preferredPart;
     let level = request.query.level;
-}
 
-function calculateUnit(response, unit, height, waist){
-    switch (unit){
-        
-
-    }
     
+    createRoutine(response, gender,height, waist, hours, preferredPart,level);
 }
+
+function calculateRFM(gender,height,waist){
+    console.log("enter this method");
+    let rfm = 64 - 20 * (height/waist) + 12*gender;
+    
+    return rfm;
+}
+
+function createRoutine(response, gender, height, waist, hours, preferredPart, level){
+    let sex= "";
+    let alert = "";
+    switch (gender){
+        case 0:
+            sex = "male";
+            break;
+        case 1:
+            sec ="female";
+            break;
+    }
+    let rfm = calculateRFM(gender,height, waist);
+    if (rfm >= 50 || rfm <=0){
+        alert ="Please make sure you input height and waist circumference with same unit";
+    }
+    let resultrfm = rfm+"%";
+    console.log(resultrfm);
+    let startLevel = 0;
+
+    switch (level){
+        case 'warmup':
+            startLevel = "1";
+            break;
+        case 'basic':
+            startlevel = "2";
+            break;
+        case 'hard':
+            startlevel = "4";
+            break;
+        case 'intense':
+            startlevel = "6";
+            break;
+    }
+
+    var sql = "select c.name, l.level, repetition, setcount, weight, repetitionweighted, setcountweighted from chesttbl c join routinetbl r on c.id = r.chest_id join leveltbl l on r.level = l.level where level =" + startLevel;
+    pool.query(sql, function(err, result){
+    if (err){
+        console.log("Error in query: ");
+        console.log(err);
+    }
+    console.log("Back from DB with result: ");
+    console.log(result.rows);
+    let routine = result.rows;
+
+    const results = {resultrfn: resultrfm, alert: alert, routine: routine};
+    response.render('routines',results);
+});
+
+}
+
