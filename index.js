b5f3bb654efa8dad236394808129949851784fea
jeongks <kisuk.jeong.lds@gmail.com>
2020-12-06 11:19:18 +0900

    milestone3

--- index.js
+++ index.js
@@ -3,85 +3,60 @@ const { Pool } = require('pg');
 const express = require('express');
 //enabling static path
 const path = require('path');
-const { response } = require('express');
 const PORT= process.env.PORT || 5000;
 require('dotenv').config();
-const qs = require('querystring');
 
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
 
-
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 express()
     .use(express.static(path.join(__dirname,'public')))
     .set('views',path.join(__dirname,'views'))
     .set('view engine', 'ejs')
     .post('/', (req,res) => res.render(index))
-    // .post('/calc',formInput)
-    .post('/calc',function (request, response){
-        if(request.method = 'POST'){
-            var body = '';
-
-            request.on('data', function(data){
-                if (body.length > 1e6) request.connection.destroy();
-            });
-
-            request.on('end', function(){
-                var post = qs.parse(body);
-                console.log("gender: "+post.gender);
-                console.log("unit: "+post.unit);
-                console.log("height: "+post.height);
-                console.log("waist: "+post.waist);
-                console.log("hours: "+post.hours);
-                console.log("preferred: "+post.preferredPart);
-                console.log("level: "+ post.level);
-            });
-        }
-    })
+    .post('/calc', formInput)    
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 function formInput(request, response){
-    let gender = request.query.gender;
-    let unit = request.query.measureUnit;
-    let height = request.query.height;
-    let waist = request.query.waist;
-    let hours = request.query.hours;
-    let preferredPart = request.query.preferredPart;
-    let level = request.query.level;
-    createRoutine(response, gender,height, waist, hours, preferredPart,level);
+    console.log(request.json);
+    console.log(request.query);
+    console.log(request.body);
+    // let gender = request.body.gender;
+    // console.log("gender: " + gender)
+    // let unit = request.body.measureUnit;
+    // let height = request.body.height;
+    // console.log("height: "+height);
+    // let waist = request.body.waist;
+    // console.log("waist: "+waist);
+    // let hours = request.body.hours;
+    // console.log("hours: "+hours);
+    // let preferredPart = request.body.preferredPart;
+    // let level = request.body.level;
+    // createRoutine(response, gender,height, waist, hours, preferredPart,level);
 }
-
 function calculateRFM(gender,height,waist){
-    console.log("Enter calculateRFM()");
     let rfm = 64 - 20 * (height/waist) + 12*gender;
-    
     return rfm;
 }
 
 function createRoutine(response, gender, height, waist, hours, preferredPart, level){
-    console.log("Enter createRoutine()");
-    console.log("gender: "+gender);
-    console.log("height: "+height);
-    console.log("waist: "+waist);
     let sex= "";
     let alert = "";
     switch (gender){
-        case 0:
+        case "0":
             sex = "male";
             break;
-        case 1:
-            sec ="female";
+        case "1":
+            sex ="female";
             break;
     }
     let rfm = calculateRFM(gender,height, waist);
     if (rfm >= 50 || rfm <=0){
         alert ="Please make sure you input height and waist circumference with same unit";
     }
-    let resultrfm = rfm+"%";
-    console.log(resultrfm);
-    let startLevel = 1;
-
+    let startLevel = "1";
     switch (level){
         case 'warmup':
             startLevel = "1";
@@ -98,17 +73,16 @@ function createRoutine(response, gender, height, waist, hours, preferredPart, le
     }
 
     var sql = "select c.name, l.level, repetition, setcount, weight, repetitionweighted, setcountweighted from chesttbl c "+
-    " join routinetbl r on c.id = r.chest_id join leveltbl l on r.level = l.level where level = " + startLevel;
+    " join routinetbl r on c.id = r.chest_id join leveltbl l on r.level = l.level where l.level = " + startLevel;
     pool.query(sql, function(err, result){
         if (err){
             console.log("Error in query: ");
             console.log(err);
         }
         console.log("Back from DB with result: ");
-        console.log(response.rows);
         let routines = response.rows;
-
-        const results = {resultrfn: resultrfm, alert: alert, routine: routines};
+        console.log(result.rows);
+        const results = {rfm: rfm, alert: alert, routine: result.rows};
         response.render('routines',results);
     });
     // pool.query(sql)
